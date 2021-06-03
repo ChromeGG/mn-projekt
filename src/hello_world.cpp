d35cfa52a667a3ef74c05c4c8eeb4085309b2507
ChromeGG <kontakt.tkaczyk@gmail.com>
2021-06-03 10:36:58 +0200

    init

---
+++ src/hello_world.cpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  random_number_generator.cpp
+ *
+ *    Description:  Napisz program do generowania liczb pseudolosowych o zadanej numerycznie funkcji prawdopodobieństwa
+ *
+ *        Version:  1.0
+ *        Created:  19.03.2019 22:23:08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Adam Tkaczyk (AT), 
+ *   Organization:  PUW
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main() {
+	std::cout << "C++ example hello world application" << std::endl;
+}
