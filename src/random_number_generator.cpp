814a69310afbc1d44fa3234d4983bbfd4001ef89
ChromeGG <kontakt.tkaczyk@gmail.com>
2021-06-05 16:35:56 +0200

    code refactor

--- src/random_number_generator.cpp
+++ src/random_number_generator.cpp
@@ -3,16 +3,15 @@
  *
  *       Filename:  random_number_generator.cpp
  *
- *    Description:  Napisz program do generowania liczb pseudolosowych o
- zadanej
- * numerycznie funkcji prawdopodobieństwa
+ *    Description:  Napisz program do generowania liczb pseudolosowych o zadanej
+ *                  numerycznie funkcji prawdopodobieństwa
  *
  *        Version:  1.0
- *        Created:  19.03.2019 22:23:08
+ *        Created:  19.03.2021 16:23:08
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Adam Tkaczyk (AT),
+ *         Author:  Adam Tkaczyk, Piotr Pakulski, Marcin Makarewicz
  *   Organization:  PUW
  *
  * =====================================================================================
@@ -23,21 +22,21 @@
 
 using namespace std;
 
-// D. Knuth variables
-const long a = 3141592653;
-const long c = 2718281829;
-const long m = 34359738368;
+// Donald Knuth variables
+const long m = 34359738368;  // 2^35
+const long a = 3141592653;   // π * 10^9
+const long c = 2718281829;   // e * 10^9
 
 long seed;
 
 // a * b mod n
 long multiply(long a, long b, long n) {
-    long m, w;
+    long m = 0, w = 0;
 
-    w = 0;
-    m = 1;
     while (m) {
-        if (b & m) w = (w + a) % n;
+        if (b & m) {
+            w = (w + a) % n;
+        }
         a = (a << 1) % n;
         m <<= 1;
     }
@@ -53,7 +52,7 @@ long LCG() {
 int main() {
     long min, max, result, rangeLength;
     int n;
-    double XR;
+    double realResult;
     cout << "Input seed:" << endl;
     cin >> seed;
     cout << "Input lower range:" << endl;
@@ -64,11 +63,13 @@ int main() {
     cin >> n;
 
     rangeLength = max - min + 1;
+
     cout << "Results:" << endl;
     for (int i = 0; i < n; i++) {
-        XR = LCG() / (double)m;
-        result = (long)floor(XR * rangeLength) + min;
+        realResult = LCG() / (double)m;
+        result = floor(realResult * rangeLength) + min;
         cout << result << " ";
     }
+
     return 0;
 }
